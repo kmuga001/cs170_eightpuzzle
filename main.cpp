615df7269eb63b5011777125c2ee0d9872d7ee16
Kinjal Mugatwala <kmuga001@ucr.edu>
2021-02-18 19:17:31 -0800

    added ability to move in 4 directions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//#include "state.h"
+#include "state.h"
 using namespace std;
 
 void displayGridState(vector <vector<int> > state);
+State* moveUp_State(State* stateNode);
+State* moveDown_State(State* stateNode);
+State* moveLeft_State(State* stateNode);
+State* moveRight_State(State* stateNode);
+vector<int> getZeroPosition(vector <vector<int> > state);
+
 
 int main() {
     /*
@@ -39,6 +45,20 @@ int main() {
     initial_state.push_back(row_3);
     
     displayGridState(initial_state);
+
+    State* initState = new State(initial_state);
+    //cout << initState->getGrid() << endl;
+    vector< vector<int> > temp = initState->getGrid();
+    
+
+    //checking if player can move
+    State* check = moveRight_State(initState);
+    cout << endl << endl;
+    if(check->rightChild == 0) {
+        cout << "no child, so no grid" << endl;
+    } else {
+        displayGridState((check->rightChild)->getGrid());
+    }
     
 
     return 0;
@@ -54,5 +74,150 @@ void displayGridState(vector <vector<int> > state) {
         }
         cout << endl;
     }
+    cout << endl;
 
+}
+
+vector<int> getZeroPosition(vector <vector<int> > state) {
+    vector<int> ind;
+    for(int i = 0; i < state.size(); i++){
+        for(int j = 0; j < state[i].size(); ++j){
+            if(state[i][j] == 0){
+                ind.push_back(i);
+                ind.push_back(j);
+                
+                return ind;
+            }
+        }
+    }
+    
+    ind.push_back(10);
+    ind.push_back(10);
+    return ind;
+}
+
+State* moveUp_State(State* stateNode) {
+    vector<int> ind = getZeroPosition(stateNode->getGrid());
+    
+    cout << ind[0] << ", " << ind[1] << endl;
+    if(ind[0] == 10 && ind[1] == 10) {
+        cout << "Could not find 0" << endl; //is an error, address later
+        
+    } 
+
+    //cannot move up if 0 is on the top row (which is row = 0)
+    if(ind[0] == 0) { //is on the top row, 0 can't move
+        cout << "CAN'T MOVE UP" << endl;
+        //leave statenode's upchild as null in default because it can't move up
+    } else {
+        //it can move up and so we can make a new grid and state object
+        //use same grid from parameter and copy it over
+        //swap 0 with position above
+        vector< vector<int> > newMoveGrid = stateNode->getGrid();
+        int temp = newMoveGrid[ind[0]][ind[1]]; //0's position
+        newMoveGrid[ind[0]][ind[1]] = newMoveGrid[ind[0] - 1][ind[1]];
+        newMoveGrid[ind[0] - 1][ind[1]] = temp;
+
+        //create a new State object with this new created grid after making move
+        State* upGrid = new State(newMoveGrid);
+        stateNode->upChild = upGrid;
+
+    }
+
+    return stateNode;
+    
+}
+
+
+State* moveDown_State(State* stateNode) {
+    vector<int> ind = getZeroPosition(stateNode->getGrid());
+    
+    cout << ind[0] << ", " << ind[1] << endl;
+    if(ind[0] == 10 && ind[1] == 10) {
+        cout << "Could not find 0" << endl; //is an error, address later
+        
+    } 
+
+    //cannot move down if 0 is on the bottom row (which is row = 2)
+    if(ind[0] == 2) { //is on the bottom row, 0 can't move
+        cout << "CAN'T MOVE DOWN" << endl;
+        //leave statenode's downchild as null in default because it can't move down
+    } else {
+        //it can move down and so we can make a new grid and state object
+        //use same grid from parameter and copy it over
+        //swap 0 with position below
+        vector< vector<int> > newMoveGrid = stateNode->getGrid();
+        int temp = newMoveGrid[ind[0]][ind[1]]; //0's position
+        newMoveGrid[ind[0]][ind[1]] = newMoveGrid[ind[0] + 1][ind[1]];
+        newMoveGrid[ind[0] + 1][ind[1]] = temp;
+
+        //create a new State object with this new created grid after making move
+        State* downGrid = new State(newMoveGrid);
+        stateNode->downChild = downGrid;
+
+    }
+
+    return stateNode;
+}
+
+State* moveLeft_State(State* stateNode) {
+    vector<int> ind = getZeroPosition(stateNode->getGrid());
+    
+    cout << ind[0] << ", " << ind[1] << endl;
+    if(ind[0] == 10 && ind[1] == 10) {
+        cout << "Could not find 0" << endl; //is an error, address later
+        
+    } 
+
+    //cannot move left if 0 is on the first column (col = 0)
+    if(ind[1] == 0) { //is on the first col, 0 can't move
+        cout << "CAN'T MOVE LEFT" << endl;
+        //leave statenode's leftchild as null in default because it can't move left
+    } else {
+        //it can move left and so we can make a new grid and state object
+        //use same grid from parameter and copy it over
+        //swap 0 with position to the left
+        vector< vector<int> > newMoveGrid = stateNode->getGrid();
+        int temp = newMoveGrid[ind[0]][ind[1]]; //0's position
+        newMoveGrid[ind[0]][ind[1]] = newMoveGrid[ind[0]][ind[1] - 1];
+        newMoveGrid[ind[0]][ind[1] - 1] = temp;
+
+        //create a new State object with this new created grid after making move
+        State* leftGrid = new State(newMoveGrid);
+        stateNode->leftChild = leftGrid;
+
+    }
+
+    return stateNode;
+}
+
+State* moveRight_State(State* stateNode) {
+    vector<int> ind = getZeroPosition(stateNode->getGrid());
+    
+    cout << ind[0] << ", " << ind[1] << endl;
+    if(ind[0] == 10 && ind[1] == 10) {
+        cout << "Could not find 0" << endl; //is an error, address later
+        
+    } 
+
+    //cannot move right if 0 is on the third column (col = 2)
+    if(ind[1] == 2) { //is on the first col, 0 can't move
+        cout << "CAN'T MOVE RIGHT" << endl;
+        //leave statenode's rightchild as null in default because it can't move right
+    } else {
+        //it can move right and so we can make a new grid and state object
+        //use same grid from parameter and copy it over
+        //swap 0 with position to the right
+        vector< vector<int> > newMoveGrid = stateNode->getGrid();
+        int temp = newMoveGrid[ind[0]][ind[1]]; //0's position
+        newMoveGrid[ind[0]][ind[1]] = newMoveGrid[ind[0]][ind[1] + 1];
+        newMoveGrid[ind[0]][ind[1] + 1] = temp;
+
+        //create a new State object with this new created grid after making move
+        State* rightGrid = new State(newMoveGrid);
+        stateNode->rightChild = rightGrid;
+
+    }
+
+    return stateNode;
 }
--- state.h
+++ state.h
@@ -1,6 +1,5 @@
 #ifndef STATE_H
 #define STATE_H
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,15 +9,23 @@ using namespace std;
 
 class State {
     private:
-        vector< vector<int>> gridState;
+        vector< vector<int> > gridState;
+        /*
+        State* upChild;
+        State* downChild;
+        State* leftChild;
+        State* rightChild;*/
+
+    public:
         State* upChild;
         State* downChild;
         State* leftChild;
         State* rightChild;
+        State(vector<vector <int> > &gState): gridState(gState){upChild = 0; downChild = 0; leftChild = 0; rightChild = 0;};
 
-    public:
-        State(vector<vector <int>> gState);
-        //State(); --> not sure if needed yet
+        vector< vector<int> > getGrid() { return this->gridState; }
+        
+        //~State(); --> not sure if need destructor yet
 };
 
 #endif
